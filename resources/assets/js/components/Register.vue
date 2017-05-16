c8fe0fa1aea1af49a94fb3121c4c482fc1272e69
Ahmed Ashraf <ahmed29329@gmail.com>
2017-05-17 00:26:40 +0200

    init

---
+++ resources/assets/js/components/Register.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Login</div>
+                    <div class="panel-body">
+                        <div class="form-group">
+                            <label for="name" class="col-md-4 control-label">Full Name</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="name" class="form-control" v-model="name" required autofocus>
+                            </div>
+                            <br><br>
+                        </div>
+                        <div class="form-group">
+                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control" v-model="email" required>
+                            </div>
+                            <br><br>
+                        </div>
+                        <div class="form-group">
+                            <label for="password" class="col-md-4 control-label">Password</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control" v-model="password" required>
+                            </div>
+                            <br><br>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-8 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Create Account
+                                </button>
+
+                                <a class="btn btn-link" href="Login">
+                                    Login
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                name: '',
+                email: '',
+                password: ''
+            }
+        }
+    }
+</script>
